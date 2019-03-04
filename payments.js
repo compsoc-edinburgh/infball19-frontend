1dade7115909656f98415051ed0c6cec8589f83b
Patrick Kage <quadnix1@gmail.com>
2019-03-04 05:05:25 +0000

    added pre-validation step

--- payments.js
+++ payments.js
@@ -1,5 +1,7 @@
 // pk_test_WLvFPL1mC9cr94Sjj6wxvBPN
 
+const API_URL = '/'
+
 const stripe   = Stripe('pk_test_WLvFPL1mC9cr94Sjj6wxvBPN')
 const elements = stripe.elements()
 const card     = elements.create('card', {
@@ -23,45 +25,44 @@ card.addEventListener('change', ({error}) => {
 
 // form mgmt
 
-const collectAndSubmit = (token) => {
-	// visual state
-	window.scrollTo({
-		'behavior': 'smooth',
-		'left': 0,
-		'top': document.querySelector('.form-container').offsetTop - 100
-	})
-	setTimeout(() => document.querySelector('.form-flipper').classList.add('form-flipper--flipped'), 750)
-
+const collectFields = () => {
 	const extract = name => document.querySelector(`[name=${name}]`)
 	let dietreqs = ['glutenfree', 'lactosefree', 'vegan', 'vegetarian'].map(n => extract(n).checked ? n : null).filter(n => n !== null)
 
 	const fields = {
-		token: token.id,
 		uun: extract('uun').value,
 		email: extract('email').value,
 		fullname: extract('full-name').value,
 		noalcohol: extract('noalcohol').checked,
 		specialreqs: extract('othernotes').value,
+		staffcode: extract('invitecode').value,
 		mealtype: dietreqs
 	}
-	if (extract('invitecode').value !== '') {
-		fields.staffcode = extract('invitecode').value
-	}
 
-	const clearIndefinite = () => document.querySelector('.form-outcome--indefinite').remove()
-	const onsuccess = (id) => {
-		clearIndefinite()
-		document.querySelector('.form-outcome--successful').classList.remove('form-outcome--hidden')
-		document.querySelector('#success-id').textContent = id
-	}
-	const onfailure = (msg) => {
-		clearIndefinite()
-		document.querySelector('.form-outcome--unsuccessful').classList.remove('form-outcome--hidden')
-		document.querySelector('#failure-message').textContent = msg
-	}
+	return fields
+
+}
+
+const formClearIndefinite = () => document.querySelector('.form-outcome--indefinite').remove()
+const formSuccess = (id) => {
+	formClearIndefinite()
+	document.querySelector('.form-outcome--successful').classList.remove('form-outcome--hidden')
+	document.querySelector('#success-id').textContent = id
+}
+const formFailure = (msg) => {
+	formClearIndefinite()
+	document.querySelector('.form-outcome--unsuccessful').classList.remove('form-outcome--hidden')
+	document.querySelector('#failure-message').textContent = msg
+}
+
+const chargeForm = (token) => {
+
+	const fields = collectFields()
+	fields.token = token.id // patch in the ID
 
 
-	fetch('/charge', {
+
+	fetch(`${API_URL}/charge`, {
 		method: 'POST',
 		cache: 'no-cache',
 		headers: {
@@ -73,15 +74,41 @@ const collectAndSubmit = (token) => {
 		.then(r => {
 			console.log(r)
 			if (r.status === 'success') {
-				onsuccess(r.data)
+				formSuccess(r.data)
 			} else {
-				onfailure(r.message)
+				formFailure(r.message)
 			}
 		})
 		.catch(e => {
 			console.warn(e)
-			onfailure('A server error occurred.')
+			formFailure('A server error occurred.')
 		})
+}
+
+const validateForm = async () => {
+	const fields = collectFields()
+	let resp = null
+
+	try {
+		resp = await fetch(`${API_URL}/validate`, {
+			method: 'POST',
+			cache: 'no-cache',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify('fields')
+		}).then(r => r.json())
+	} catch (e) {
+		console.warn(e)
+		formFailure('A server error occurred.')
+	}
+
+	if (r.status === 'success') {
+		return true
+	} else {
+		formFailure(r.message)
+		return false
+	}
 }
 
 const form = document.querySelector('#ticketform')
@@ -89,6 +116,15 @@ form.addEventListener('submit', async (e) => {
 	e.preventDefault()
 	console.log('clicked submit')
 
+	// visual state
+	window.scrollTo({
+		'behavior': 'smooth',
+		'left': 0,
+		'top': document.querySelector('.form-container').offsetTop - 100
+	})
+	setTimeout(() => document.querySelector('.form-flipper').classList.add('form-flipper--flipped'), 750)
+
+	if (!(await validateForm())) return;
 
 	const {token, error} = await stripe.createToken(card)
 
@@ -96,7 +132,6 @@ form.addEventListener('submit', async (e) => {
 		const displayError = document.querySelector('#card-errors')
 		displayError.textContent = error.message
 	} else {
-		collectAndSubmit(token)
+		chargeForm(token)
 	}
-
 })
